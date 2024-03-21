9c8e2aa548f32e319a173e3dfc6c564b5beea1a5
guilhermelima <guilhermelimaruby@gmail.com>
2024-03-21 09:27:48 -0300

    pre mongo

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const controller = require('../controllers/index')
+const errorController = require('../controllers/error')
 const router = express.Router()
 
 router.get('/', controller.getHomePage)
-
 router.get('/signup', controller.signUp)
-
 router.get('/members', controller.showMembersPage)
-
 router.get('/admin', controller.admin)
-
-router.use((req, res, next) => {
-    res.status(404).render('404');
-});
+router.use(errorController.get404Page);
 
 module.exports = router
