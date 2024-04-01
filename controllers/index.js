4e9e356869a8a715cbc6b2c5235b97c50d9e3352
guilhermelima <guilhermelimaruby@gmail.com>
2024-04-01 15:38:13 -0300

    expression-section

--- controllers/index.js
+++ controllers/index.js
@@ -9,6 +9,7 @@ exports.login = async (req, res, next) => {
     try {
         const user = await User.findUserBy(email, password);
         if (user) {
+            req.session.user = user; // Armazena o usuário na sessão
             res.redirect('/members');
         } else {
             res.render('index');
@@ -40,6 +41,7 @@ exports.signUp = (req, res, next) => {
 }
 
 exports.showMembersPage = (req, res) => {
+    // console.log(req.session.user);
     res.render('members')
 }
 
@@ -49,4 +51,22 @@ exports.admin = (req, res) => {
 
 exports.get404Page = (req, res, next) => {
     res.status(404).render('404')
+}
+
+
+exports.checkAuth = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next()
+    } else {
+        res.redirect('/')
+    }
+}
+
+exports.logout = (req, res, next) => {
+    req.session.destroy( err => {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/')
+    })
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('../controllers/index');
+const session = require('express-session');
 const router = express.Router();
+
 router.use(bodyParser.urlencoded({ extended: true }));
+router.use(session({
+    secret: 'abc123',
+    resave: false,
+    saveUninitialized: false
+}))
 
 router.get('/', controller.showIndex);
 router.post('/', controller.login);
 router.get('/signup', controller.showPageSignUp);
 router.post('/signup', controller.signUp);
-router.get('/members', controller.showMembersPage);
+router.get('/members', controller.checkAuth, controller.showMembersPage);
+router.get('/logout', controller.logout)
 router.get('/admin', controller.admin);
 router.use(controller.get404Page);
 
